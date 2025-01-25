198f031e4c81151e7d2f42a629058efc52b7ad47
MariamSayeeed <ms1023993979@gmail.com>
2025-01-25 11:03:58 +0200

    updated Demo

---
+++ Demo/Helper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Helper
+    {
+        // Passing By Value
+
+
+        //public static void Swap (int x, int y)
+        //{
+        //    int Temp = x;
+        //    x = y;
+        //    y = Temp;
+
+        //} 
+
+        // Passing By Refrance
+        //public static void Swap(ref int x, ref int y)
+        //{
+        //    int Temp = x;
+        //    x = y;
+        //    y = Temp;
+
+
+
+        //}
+
+        //public static void Swap(ref double x, ref double y)
+        //{
+        //    double Temp = x;
+        //    x = y;
+        //    y = Temp;
+
+
+
+        //}
+
+        //public static void Swap (ref float x, ref int y)
+        //{
+        //    double Temp = x;
+        //    x = y;
+        //    y = Temp;
+
+
+
+        //}
+
+    }
+}
